65aa30ee1376e9a37da3a7d8e97bcf2b8cfbe6c5
unknown <tecmatheusg@hotmail.com>
2023-08-07 20:43:48 -0300

    Alterações no carousel

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,16 +5,17 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <nav className="navbar navbar-expand-lg bg-light">
                 <div className="container-fluid">
+                <Link to="/">
+                    <img width="40" className="ms-lg-4" src={logo} />
+                </Link>
+                <Link className="navbar-brand ps-lg-3" to="/">MJA Instalações</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                        <a className="navbar-brand" href="#">
-                            <img src={logo} alt="Logo" width="40" className="d-inline-block" />
-                            MJA Instalações
-                        </a>
+                        
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li className="nav-item">
                                 <Link className="nav-link active" aria-current="page" to="/">Início</Link>
--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -3,23 +3,23 @@ import img1 from '../../img/vertical/img-vert1.jpeg'
 import img2 from '../../img/vertical/img-vert2.jpeg'
 
 export default function Sobre() {
-    const tempoSlider = 3000;
+    const tempoCarousel = 3000;
+    const tamanhoBotaoCarousel = 40;
 
     return (
         <>
             <div className='container col-12 mt-5'>
-
                 <div className='d-flex align-items-center'>
-                    <div className='col-3'>
+
+                    <div className='col-2 d-flex justify-content-center'>
                         <button className="BOTAO1 bg-transparent border-0" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="black" class="bi bi-chevron-left" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width={tamanhoBotaoCarousel} height={tamanhoBotaoCarousel} fill="black" class="bi bi-chevron-left" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
                             </svg>
                             <span className="visually-hidden">Previous</span>
                         </button>
                     </div>
-                    <div className='col-6'>
+                    <div className='col-8'>
                         <div id="carouselExampleCaptions" className="carousel slide" data-bs-theme="dark">
                             <div className="carousel-indicators">
                                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
@@ -31,35 +31,29 @@ export default function Sobre() {
                                         <img src={img1} className="d-block w-50" alt="..." />
                                         <div className="d-flex flex-column justify-content-end mb-5 ps-3">
                                             <div className='fs-3'>Instalação de aabbbcccdd</div>
-                                            <div className='fs-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dapibus libero id purus lobortis faucibus. In neque dolor, consequat ut tristique quis, facilisis at ex. In ac bibendum nisl, ac dignissim turpis. Phasellus pharetra arcu ut laoreet egestas. Proin condimentum mauris vel diam congue, sed cursus risus mollis. Sed non mi sem. Aliquam vitae metus in risus sodales ultrices. In hac habitasse platea dictumst. Curabitur lectus velit, viverra eu erat in, dapibus pharetra dolor.</div>
+                                            <div className='fs-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dapibus libero id purus.</div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="carousel-item" >
+                                    <div className='d-flex'>
+                                        <img src={img2} className="d-block w-50" alt="..." />
+                                        <div className="d-flex flex-column justify-content-end mb-5 ps-3">
+                                            <div className='fs-3'>Instalação de aabbbcccdd</div>
+                                            <div className='fs-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dapibus libero id purus.</div>
                                         </div>
                                     </div>
                                 </div>
 
 
-                                <div className="carousel-item" >
-                                    <img src={img2} className="d-block w-50" alt="..." />
-                                    <div className="carousel-caption d-none d-md-block">
-                                        <h5>Third slide label</h5>
-                                        <p>Some representative placeholder content for the third slide.</p>
-                                    </div>
-                                </div>
-
-
                             </div>
-                            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span className="visually-hidden">Previous</span>
-                            </button>
-                            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span className="visually-hidden">Next</span>
-                            </button>
+
                         </div>
                     </div>
-                    <div className='col-3'>
-                        <button className="BOTAO2 bg-transparent border-0" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">                            
-                            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="black" class="bi bi-chevron-right" viewBox="0 0 16 16">
+                    <div className='col-2 d-flex justify-content-center'>
+                        <button className="BOTAO2 bg-transparent border-0" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                            <svg xmlns="http://www.w3.org/2000/svg" width={tamanhoBotaoCarousel} height={tamanhoBotaoCarousel} fill="black" class="bi bi-chevron-right" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
                             </svg>
                             <span className="visually-hidden">Next</span>
